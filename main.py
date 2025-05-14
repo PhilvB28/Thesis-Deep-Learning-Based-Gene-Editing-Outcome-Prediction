2aff16fb19b6ffdb49df9c42816b9d7e77d0df81
pvblohn1 <blohn@rhrk.uni-kl.de>
2025-05-14 20:34:18 +0200

    Update main

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ from sklearn.model_selection import train_test_split
 from train import train_valid
 from test import test
 from trainsoft import train_valid_soft
-from train_marginloss import train_valid_marginloss
 
-from models.CRISPR_Caps_hard import CapsNetRegressorHardSharing_3_layer
-from models.CRISPR_Caps_soft import CapsNetRegressorSoftSharing_3layer
-from models.Capstest_marginloss import CapsNetRegressorHardSharingMargin
+from models.CRISPR_Caps_hard import CrisprCaps_hard
+from models.CRISPR_Caps_soft import CrisprCaps_soft
 
 
 seed = 42
@@ -26,16 +24,15 @@ def set_seed(seed):
 # Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
 if __name__ == "__main__":
     set_seed(seed)
     print(f'Using device: {device}')
 
     # Select model
-    model = CapsNetRegressorSoftSharing_3layer(input_channels=4, sequence_length=60, in_dim=5, out_dim=24, num_routing=1)
+    model = CrisprCaps_hard(input_channels=4, sequence_length=60, in_dim=16, out_dim=16, num_routing=2)
 
     # Select model params
-    params ="./saved_models/CapsNet_soft_pso.pth"
+    params ="./saved_models/CapsNet_hard.pth"
 
     # Paths
     forecast_path = './datasets/Forecast K562 (n=35129).csv'
@@ -47,27 +44,28 @@ if __name__ == "__main__":
     sprout_df = pd.read_csv(sprout_path)
     forecast_lindel_df = pd.concat([forecast_df, lindel_df], axis=0)
 
-    #_ , forecast_test_df = train_test_split(forecast_df, test_size=0.1, random_state=42)
 
     # Data
     forecast_lindel_train_df, temp_df = train_test_split(forecast_lindel_df, test_size=0.2, random_state=42)
     forecast_train_df, temp2_df = train_test_split(forecast_df, test_size=0.2, random_state=42)
+    lindel_train_df, temp3_df = train_test_split(lindel_df, test_size=0.2, random_state=42)
 
     # validation and test sets (10% each of the original data)
     forecast_lindel_valid_df, forecast_lindel_test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
     forecast_valid_df, forecast_test_df = train_test_split(temp2_df, test_size=0.5, random_state=42)
+    lindel_valid_df, lindel_test_df = train_test_split(temp3_df, test_size=0.5, random_state=42)
 
     # Run training
     print('Initializing Training...')
 
-    train_valid_soft(
-        train_df=forecast_train_df,
-        valid_df=forecast_valid_df,
+    train_valid(
+        train_df=lindel_train_df,
+        valid_df=lindel_valid_df,
         model_save_file=params,
         model=model,
-        epochs=100,
-        batch_size=32, # batch_size=128
-        lr=0.001,    # lr=7.13e-4
+        epochs=150,
+        batch_size=32,
+        lr=0.001,
         patience=10
     )
 
@@ -75,4 +73,4 @@ if __name__ == "__main__":
     print('Initializing Testing...')
     print('Testing' + params)
 
-    test([forecast_lindel_test_df, forecast_test_df, lindel_df, sprout_df], model, params, device, batch_size=32, table=True)
+    test([forecast_lindel_test_df, forecast_test_df, lindel_test_df, sprout_df], model, params, device, batch_size=32, table=True)
